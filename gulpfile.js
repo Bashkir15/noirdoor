5722485e46d9d80ac0298a08e5ce5e25fb780625
Forrest Collins <Forest.d.collins@gmail.com>
2017-04-05 22:45:58 -0500

    Added a small gulp task for sass that can be removed later but will make my life a lot easier because vanilla css is yucky

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const gutil = require('gulp-util');
+
+const sass = require('gulp-sass');
+const pure = require('gulp-purifycss');
+const autoprefixer = require('gulp-autoprefixer'); // incase we want to be nice
+
+const paths = {
+	dev: {
+		// to separate the already written css from my
+		// messing around
+		styles: './lib/static/css/compiled/',
+		mainSass: './lib/static/sass/main.sass',
+		otherSass: './lib/static/sass/**/*.sass'
+	}
+};
+
+function handleErrors(...args) {
+	gutil.beep();
+	this.emit('end');
+
+	notify.onError({
+		title: 'Error',
+		message: '<%= error.message %>'
+	}).apply(this, args);
+}
+
+gulp.task('devStyles', () => {
+	gulp.src(paths.dev.mainSass)
+		.pipe(plumber({
+			errorHandler: handleErrors
+		}))
+		.pipe(sass({
+			errLogToConsole: true,
+			outputStyle: 'compact',
+			precision: 10
+		}))
+		.pipe(autoprefixer({
+			browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
+			cascade: true
+		}))
+		.pipe(pure(['./lib/views/**/*.html', './lib/static/scripts/**/*.js']))
+		.pipe(gulp.dest(paths.dev.styles))
+		.pipe(notify('Styles Task Complete'))
+})
+
+gulp.task('default', ['devStyles'], () => {
+	gulp.watch([paths.dev.mainSass, paths.dev.otherSass], ['devStyles']);
+});
